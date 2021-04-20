4ff08300f090ff4a9d19acfd8de0cde44a09e378
Hhhr <Hhhr1214@gmail.com>
2021-04-20 09:57:36 +0800

    路由创建和登录页渲染

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import reset from './styles/reset.less'
+import router from './router/index'
 
 Vue.config.productionTip = false
 
 new Vue({
+    router,
     render: h => h(App),
 }).$mount('#app')
