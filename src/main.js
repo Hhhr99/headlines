852d1229cb0b796d6f98c90906119e418207e564
Hhhr <Hhhr1214@gmail.com>
2021-04-23 12:25:32 +0800

    首页标签项的添加

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import reset from './styles/reset.less'
 import router from './router/index'
 
-import {Toast, Icon, Uploader, Dialog, ActionSheet,Field } from "vant";
+import {Toast, Icon, Uploader, Dialog, ActionSheet,Field,Tab, Tabs } from "vant";
 
 Vue.use(Toast)
     .use(Icon)
@@ -13,6 +13,8 @@ Vue.use(Toast)
     .use(Dialog)
     .use(ActionSheet)
     .use(Field )
+    .use(Tab )
+    .use(Tabs )
 
 Vue.config.productionTip = false
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="index">
+    <!--    头部-->
     <div class="header">
       <div class="logo">
         <span class="iconfont iconnew"></span>
@@ -12,12 +13,28 @@
         <van-icon name="manager-o"/>
       </div>
     </div>
+    <!--    tab标签页-->
+    <van-tabs v-model="active" sticky swipeable>
+      <van-tab title="标签 1" style="height: 1000px;background-color: yellow;">内容 1</van-tab>
+      <van-tab title="标签 2" style="height: 1000px;background-color: red;">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
+      <van-tab title="标签 6">内容 6</van-tab>
+      <van-tab title="标签 7">内容 7</van-tab>
+      <van-tab title="标签 8">内容 8</van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 export default {
-  name: "index"
+  name: "index",
+  data() {
+    return {
+      active: 1,
+    };
+  },
 }
 </script>
 
