8d44148d0f63ffa87e5f7fbdca8e557519408a30
Hhhr <Hhhr1214@gmail.com>
2021-04-26 20:42:23 +0800

    改造数据后实现不同栏目的新闻数据的加载

--- src/views/index.vue
+++ src/views/index.vue
@@ -6,17 +6,17 @@
         <span class="iconfont iconnew"></span>
       </div>
       <div class="search">
-        <van-icon name="search" />
+        <van-icon name="search"/>
         <span>搜索商品</span>
       </div>
       <div class="user">
-        <van-icon name="manager-o" />
+        <van-icon name="manager-o"/>
       </div>
     </div>
     <!--    tab标签页-->
     <van-tabs v-model="active" sticky swipeable>
       <van-tab :title="cate.name" v-for="(cate,index) in cateList" :key="cate.id">
-        <my_post-block v-for="post in postlist" :key="post.id" :article="post"></my_post-block>
+        <my_post-block v-for="post in postlist" :key="post.id" :post="post"></my_post-block>
       </van-tab>
     </van-tabs>
 
@@ -39,20 +39,34 @@ export default {
     }
   },
   async mounted() {
-    // 获取栏目数据
+    // 页面一加载完毕：获取栏目数据
     let res = await getCateList()
     console.dir(res);
     this.cateList = res.data.data
     // console.log(this.cateList)
 
+    // 数据改造
+    this.cateList = this.cateList.map((v) => {
+      // postList：为当前栏目所添加的用于存储这个栏目的新闻数据的数组
+      return {...v, postlist: []}
+    })
     // 获取当前被激活栏目的新闻数据
-    let id = this.cateList[this.active].id
-    this.postlist = (await getPostList(id)).data.data
+
   },
   watch: {
     async active() {
+      // 单击栏目获取当前栏目的新闻数据：关键也是获取栏目id
+      if (this.active[this.active].postlist.length === 0) {
+        let id = this.active[this.active].id
+        console.log(this.active, id)
+        this.cateList[this.active].postlist = (await getCateList(id)).data.data
+      }
+    }
+  },
+  methods: {
+    async getpost() {
       let id = this.cateList[this.active].id
-      console.log(id)
+      // console.log(id)
       this.postlist = (await getPostList(id)).data.data
       console.log(this.postlist)
     }
